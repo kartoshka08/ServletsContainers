b29672a25653740cb078f929ca00f1b385b18832
Kartoshka <kartoshka0811@gmail.com>
2024-03-18 20:21:14 +0300

    update_branch_di_java

---
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -0,0 +1,62 @@
+package ru.netology.servlet;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.netology.config.JavaConfig;
+import ru.netology.controller.PostController;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainServlet extends HttpServlet {
+
+    public static final String API_POSTS = "/api/posts";
+    public static final String API_POSTS_D = "/api/posts/\\d+";
+    private PostController controller;
+
+
+    @Override
+    public void init() {
+        final var context = new AnnotationConfigApplicationContext(JavaConfig.class);
+        controller = (PostController) context.getBean("postController");
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        // если деплоились в root context, то достаточно этого
+        try {
+            final var path = req.getRequestURI();
+            final var method = req.getMethod();
+            // primitive routing
+            if (method.equals("GET") && path.equals(API_POSTS)) {
+                controller.all(resp);
+                return;
+            }
+            if (method.equals("GET") && path.matches(API_POSTS_D)) {
+                // easy way
+                final var id = parseId(path);
+                controller.getById(id, resp);
+                return;
+            }
+            if (method.equals("POST") && path.equals(API_POSTS)) {
+                controller.save(req.getReader(), resp);
+                return;
+            }
+            if (method.equals("DELETE") && path.matches(API_POSTS_D)) {
+                // easy way
+                final var id = parseId(path);
+                System.out.println(id);
+                controller.removeById(id, resp);
+                return;
+            }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private long parseId(String path) {
+        return Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
+    }
+}
